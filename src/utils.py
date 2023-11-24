62c1b189808c2a8afcfe982a78d88aef9073e159
Juliane Bruck <bruck.juliane@gmail.com>
2023-11-23 23:20:51 -0500

    added code for prediction pipeline, app.py

--- src/utils.py
+++ src/utils.py
@@ -49,3 +49,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        # opening that file path in read bite mode and loading the pickle file
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
